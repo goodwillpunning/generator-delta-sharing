92a8a464520a2274e329a0cfddf9bb7f53e39fbe
Will Girten <will.girten@databricks.com>
2023-06-19 22:45:11 -0400

    Add file writer helper.

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,6 +1,17 @@
 'use strict';
 
-var Generator = require('yeoman-generator');
+const Generator = require('yeoman-generator');
+const parseAuthor = require('parse-author');
+const path = require('path');
+const validatePackageName = require('validate-npm-package-name');
+const localPackageJson = require('../../package.json');
+const extend = require('lodash.merge');
+
+const NODE_APP_TYPE = "Node.js (backend only)";
+const REACT_APP_TYPE = "React.js App";
+
+const NODE_TEMPLATE_PATH = "delta-sharing-app-template/**";
+const REACT_TEMPLATE_PATH = "delta-sharing-app-template/**";
 
 module.exports = class extends Generator {
   constructor(args, opts) {
@@ -13,12 +24,51 @@ module.exports = class extends Generator {
 
   // Placeholder for an init method
   initializing() {
+
     this.log('Initializing...');
+
+    // Read local `package.json`
+    this.package = this.fs.readJSON(this.destinationPath('package.json'), {});
+
+    // Stub out package info
+    this.props = {
+      name: this.package.name,
+      description: this.package.description,
+      version: this.package.version,
+      homepage: this.package.homepage,
+      repositoryName: this.options.repositoryName
+    };
+    
+    // Parse author information
+    if (typeof this.package.author === 'object') {
+
+      this.props.authorName = this.package.author.name;
+      this.props.authorEmail = this.package.author.email;
+      this.props.authorUrl = this.package.author.url;
+
+    } else if (typeof this.package.author === 'string') {
+
+      let info = parseAuthor(this.package.author);
+      this.props.authorName = info.name;
+      this.props.authorEmail = info.email;
+      this.props.authorUrl = info.url;
+
+    }
   }
 
   // Gather user input for generating a React app for Delta Sharing
   async prompting() {
     this.answers = await this.prompt([
+      {
+        type: "list",
+        name: "appType",
+        message: "Enter app type",
+        choices: [
+          NODE_APP_TYPE,
+          REACT_APP_TYPE
+        ],
+        store: true
+      },
       {
         name: "appName",
         message: "Enter a name for your app",
@@ -26,7 +76,7 @@ module.exports = class extends Generator {
         store: true
       },
       {
-        name: "description",
+        name: "appDescription",
         message: "Enter a description of the app",
         default: "An app for sharing datasets.",
         store: true
@@ -49,22 +99,60 @@ module.exports = class extends Generator {
         store: true
       },
       {
-        type: "input",
+        type: "password",
         name: "bearerToken",
-        message: "Enter the sharing token",
-        store: true
+        message: "Enter the sharing token"
       }
     ]);
   }
 
-  // Placeholder method for writing the app files
   writing() {
-    this.log("Writing app name", this.answers.endpoint);
-    this.log("Writing cool feature", this.answers.bearerToken);
+
+    var templatePath;
+    if (this.answers.appType == NODE_APP_TYPE) {
+      this.log("Writing Node.js app template...");
+      templatePath = NODE_TEMPLATE_PATH;
+    } else if (this.answers.appType == REACT_APP_TYPE) {
+      this.log("Writing React.js app template...");
+      templatePath = REACT_TEMPLATE_PATH;
+    } else {
+      throw new Error("Invalid app type detected.");
+    }
+
+    // Copy the appropriate app template
+    this.fs.copy(
+      this.templatePath(templatePath),
+      this.destinationRoot(),
+      { globOptions: { dot: true } }
+    );
+
+    // Programmatically extend the `package.json` file
+    const existingPkgJson = this.fs.readJSON(this.destinationPath('package.json'), {});
+    const newPkgJson = extend({
+      name: this.answers.appName,
+      version: '0.1.0',
+      description: this.answers.appDescription,
+      author: {
+        name: this.answers.authorName,
+        email: this.answers.authorEmail
+      },
+      keywords: ["delta-sharing"],
+      engines: {
+        npm: '>= 10.0.0'
+      }
+    }, existingPkgJson);
+
+    // Extend or create package.json file in destination path
+    this.log("Writing new `package.json` file to path.");
+    this.log(newPkgJson);
+    this.fs.extendJSON(this.destinationPath('package.json'), newPkgJson);
+
   }
 
   // Placeholder method for installing depepndencies from NPM
   installing() {
     this.log("Installing dependencies...");
+    // Deprecated. Yeoman will automatically call this once during the queue
+    // this.npmInstall();
   }
 }
